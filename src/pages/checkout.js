dd6c1ddc0dabfc6f6c3ad7b911c37f268a47e485
morningstar342 <100089006+morningstar342@users.noreply.github.com>
2023-05-27 15:31:02 +0200

    1

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -15,63 +15,61 @@ function Checkout() {
   const {session} = useSession();
   return (
   <div className='bg-gray-100'>
-      <Head>
-          <title>Checkout</title>
-      </Head>
-      <Header></Header>
+    <Head>
+        <title>Checkout</title>
+    </Head>
+    <Header/>
+    <main className='lg:flex max-w-screen-2xl mx-auto'>
+      {/* Left Section */}
+      <div className='flex-grow m-5 shadow-sm'>
+        <Image
+            src='https://links.papareact.com/dyz'
+            width={1020}
+            height={250}
+            objectFit='contain'
+            className='cursor-pointer'
+        ></Image>
 
-      <main className='lg:flex max-w-screen-2xl mx-auto'>
-          {/* Left Section */}
-          <div className='flex-grow m-5 shadow-sm'>
-              <Image
-                  src='https://links.papareact.com/dyz'
-                  width={1020}
-                  height={250}
-                  objectFit='contain'
-                  className='cursor-pointer'
-              ></Image>
+        <div className='flex flex-col p-5 space-y-10 bg-white'>
+            <h1 className='text-3xl border-b pb-4 font-bold'>
+                {items.length === 0 ? 'Your Basket is Empty' : 'Shopping Basket'}
+            </h1>
 
-              <div className='flex flex-col p-5 space-y-10 bg-white'>
-                  <h1 className='text-3xl border-b pb-4 font-bold'>
-                      {items.length === 0 ? 'Your Basket is Empty' : 'Shopping Basket'}
-                  </h1>
+            {items.map((item, i) => (
+                <CheckoutProduct
+                    key={i}
+                    id={item.id}
+                    title={item.title}
+                    rating={item.rating}
+                    price={item.price}
+                    description={item.description}
+                    category={item.category}
+                    image={item.image}
+                    hasPrime={item.hasPrime}
+                >
+                </CheckoutProduct>
+            ))}
+        </div>
+      </div>
 
-                  {items.map((item, i) => (
-                      <CheckoutProduct
-                          key={i}
-                          id={item.id}
-                          title={item.title}
-                          rating={item.rating}
-                          price={item.price}
-                          description={item.description}
-                          category={item.category}
-                          image={item.image}
-                          hasPrime={item.hasPrime}
-                      >
-                      </CheckoutProduct>
-                  ))}
-              </div>
-          </div>
-
-          {/* Right Section */}
-          <div className='flex flex-col bg-white p-10 shadow-md'>
-              {items.length > 0 && (
-                  // like div
-                  <>
-                      <h2 className='whitespace-nowrap'>Subtotal ({ items.length } items):
-                          <span className='font-bold'>
-                              <Currency quantity={total} currency='USD'></Currency>
-                          </span>
-                      </h2>
-                      <button disabled={!session}
-                      className={!session ? 'btn-2 mt-2 cursor-not-allowed' : 'btn-1 mt-2'}>
-                          {!session ? 'Sign In to Checkout' : 'Proceed to Checkout'}
-                      </button>
-                  </>
-              )}
-
-          </div>
-      </main>
+      {/* Right Section */}
+      <div className='flex flex-col bg-white p-10 shadow-md'>
+        {items.length > 0 && (
+          // like div
+          <>
+            <h2 className='whitespace-nowrap'>Subtotal ({ items.length } items):
+                <span className='font-bold'>
+                    <Currency quantity={total} currency='USD'></Currency>
+                </span>
+            </h2>
+            <button disabled={!session}
+            className={!session ? 'btn-2 mt-2 cursor-not-allowed' : 'btn-1 mt-2'}>
+                {!session ? 'Sign In to Checkout' : 'Proceed to Checkout'}
+            </button>
+          </>
+        )}
+      </div>
+    </main>
   </div>
   )
 }
