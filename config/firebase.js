8e01f59e6ba3a5da575429b96806723a5254ef47
morningstar342 <100089006+morningstar342@users.noreply.github.com>
2023-05-27 14:19:30 +0200

    1

--- config/firebase.js
+++ config/firebase.js
@@ -1,30 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
-import { getFirestore } from 'firebase/firestore'
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDPVsJ4g1dzFOK1a6wYUU53FXCc1BQWwmE",
-  authDomain: "fire-src1.firebaseapp.com",
-  projectId: "fire-src1",
-  storageBucket: "fire-src1.appspot.com",
-  messagingSenderId: "980136207371",
-  appId: "1:980136207371:web:5e6b46d7fa1baef534d30e",
-  measurementId: "G-T2M0LJTCC8"
+    apiKey: "AIzaSyBnilAJiW6oqfogfxnW6pFTflp7VTpZRKA",
+    authDomain: "amz-2-84908.firebaseapp.com",
+    projectId: "amz-2-84908",
+    storageBucket: "amz-2-84908.appspot.com",
+    messagingSenderId: "713055793273",
+    appId: "1:713055793273:web:87b01fe64850ec41cca4a8",
+    measurementId: "G-6C6DLZ9MH3"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = app.name && typeof window !== 'undefined' ? getAnalytics(app) : null;
-
-const auth = getAuth(app);
-const googleProvider = new GoogleAuthProvider();
-const db = getFirestore(app);
-
-export { auth, analytics, googleProvider, db }
+const analytics = getAnalytics(app);
