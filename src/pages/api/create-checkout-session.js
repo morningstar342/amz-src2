919ac18959544b527e2426a77b7c719515b6088b
morningstar342 <100089006+morningstar342@users.noreply.github.com>
2023-05-27 21:00:23 +0200

    1

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -2,7 +2,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 export default async(req, res) =>{
   const { items, email} =req.body;
-
+  console.log( items, email)
   const transformedItems = items.map((item) => ({
     description: item.description,
     // More quanities need change
@@ -14,7 +14,7 @@ export default async(req, res) =>{
         name: item.title,
         images: [item.image]
       },
-    }
+    },
   }));
 
   const session = new stripe.checkout.sessions.create({
